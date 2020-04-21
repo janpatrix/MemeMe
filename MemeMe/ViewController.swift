dba6551dcf7d76f07a0fffef37f8be2f6154d110
Patrick Gross <patrick.gross@mobimeo.com>
2020-04-21 15:19:10 +0200

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Patrick Groß on 21.04.20.
+//  Copyright © 2020 Patrick Groß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
