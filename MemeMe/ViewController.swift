3c49065b714f46057ad8e3ba8dce030f053bef02
Patrick Gross <patrick.gross@mobimeo.com>
2020-04-21 16:48:22 +0200

    First commit with Camera and Photo Gallery actions and 2 Textfields Top and Bottom

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var cameraButton: UIBarButtonItem!
+	
+	let imagePicker = UIImagePickerController()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
 	}
 
-
+	@IBAction func pickPicture(_ sender: UIBarButtonItem) {
+		
+		
+		imagePicker.delegate = self
+		imagePicker.allowsEditing = true
+		imagePicker.mediaTypes = ["public.image"]
+		imagePicker.sourceType = .photoLibrary
+		
+		present(imagePicker, animated: true, completion: nil)
+		
+	}
+	
+	@IBAction func takePhoto(_ sender: Any) {
+		
+		imagePicker.delegate = self
+		imagePicker.allowsEditing = true
+		imagePicker.mediaTypes = ["public.image"]
+		imagePicker.sourceType = .camera
+		
+		present(imagePicker, animated: true, completion: nil)
+	}
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		
+		  if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			   imageView.contentMode = .scaleAspectFit
+			   imageView.image = pickedImage
+		   }
+		
+		   dismiss(animated: true, completion: nil)
+	}
 }
+
 
